6066bce65de6e2e3ed0b3a23f8a09694e2d81fc2
Wouter Lefever <wouter.lefever@tomtom.com>
2019-03-04 21:49:29 +0100

    hackerrank challenge loops

--- src/main/java/Challenge6/Solution.java
+++ src/main/java/Challenge6/Solution.java
@@ -3,5 +3,25 @@ package Challenge6;
 /**
  * @author lefeverw
  */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.IntStream;
+
 public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        IntStream.rangeClosed(1,10).forEach(number-> System.out.printf("%d x %d = %d\n",N,number,N*number));
+        scanner.close();
+    }
 }
---
+++ src/main/java/Challenge7/Solution.java
@@ -0,0 +1,27 @@
+package Challenge7;
+
+/**
+ * @author lefeverw
+ */
+import java.util.*;
+import java.io.*;
+import java.util.stream.IntStream;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            IntStream.rangeClosed(0,n-1).forEach(value->{
+                System.out.printf("%d ",IntStream.rangeClosed(0,value).map(exp->(int)Math.pow(2,exp)).sum()*b+a);
+            });
+            System.out.println();
+        }
+
+        in.close();
+    }
+}
+
