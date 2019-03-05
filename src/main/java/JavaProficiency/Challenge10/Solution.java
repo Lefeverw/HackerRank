47d7394c9d8319938cc4bf282b4b44f02db4a3f2
Wouter Lefever <wouter.lefever@tomtom.com>
2019-03-05 20:37:15 +0100

    some more challenges

---
+++ src/main/java/JavaProficiency/Challenge10/Solution.java
@@ -0,0 +1,36 @@
+package JavaProficiency.Challenge10;
+
+/**
+ * @author lefeverw
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    static boolean flag;
+    static int B;
+    static int H;
+    static {
+        final Scanner scanner = new Scanner(System.in);
+        B = scanner.nextInt();
+        H = scanner.nextInt();
+        flag= false;
+        if(B<=0 ||H<=0)
+                System.out.println("java.lang.Exception: Breadth and height must be positive");
+
+        else
+            flag = true;
+    }
+
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
---
+++ src/main/java/JavaProficiency/Challenge3/Solution.java
@@ -0,0 +1,23 @@
+package JavaProficiency.Challenge3;
+
+/**
+ * @author lefeverw
+ */
+import java.util.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N%2==1||(N>=5&&N<=20))
+            System.out.println("Weird");
+        else
+            System.out.println("Not Weird");
+        scanner.close();
+    }
+}
