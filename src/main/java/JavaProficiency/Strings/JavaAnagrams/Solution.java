bf94e90b25cc97155cb593be1062d2a67c829f58
Wouter Lefever <wouter.lefever@tomtom.com>
2019-03-06 20:43:13 +0100

    diagonal difference

--- src/main/java/JavaProficiency/Strings/JavaAnagrams/Solution.java
+++ src/main/java/JavaProficiency/Strings/JavaAnagrams/Solution.java
@@ -3,5 +3,35 @@ package JavaProficiency.Strings.JavaAnagrams;
 /**
  * @author lefeverw
  */
+import java.util.Scanner;
+
 public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        String[] lettersOfA = a.toLowerCase().split("");
+        String[] lettersOfB = b.toLowerCase().split("");
+        int counter = 0;
+        for (int i = 0; i < lettersOfA.length; i++) {
+            for (int j = 0; j < lettersOfB.length; j++) {
+                if(lettersOfB[j].equals(lettersOfA[i])){
+                    lettersOfB[j]="";
+                    lettersOfA[i]="";
+                    counter++;
+                    break;
+                }
+            }
+        }
+        return counter==a.length()&&counter==b.length();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
 }
---
+++ src/main/java/ProblemSolving/DiagonalDifference/Solution.java
@@ -0,0 +1,54 @@
+package ProblemSolving.DiagonalDifference;
+
+/**
+ * @author lefeverw
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr) {
+        int firstdiagonal =IntStream.range(0,arr.length).map(index->arr[index][index]).sum();
+        int seconddiagonal = IntStream.range(0,arr.length).map(index->arr[index][arr.length-1-index]).sum();
+return Math.abs(firstdiagonal-seconddiagonal);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
